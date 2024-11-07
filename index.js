07c1af267832131ca7cbbc10749163e770cad248
jahid9982 <rayhan99982@gmail.com>
2024-11-07 23:41:31 +0600

    first commi

---
+++ index.js
@@ -0,0 +1,75 @@
+// 1. 
+// let number = 0;
+// while(number<=60) {
+//     console.log("I will invest at least 6 hrs every single day for next 60 days!")
+//     number++
+// }
+
+// 2. 
+// let num = 61;
+// while(num<100){
+//     console.log(num)
+//     num = num+2
+// }
+// let num = 78;
+// while(num<=98){
+//     console.log(num)
+//     num = num+2
+// }
+
+
+// let num = 81;
+// let sum = 0;
+// while(num<=131){
+//     // console.log(num)
+//     num = num+2;
+//     sum = sum+num;
+// }
+// console.log(sum)
+
+
+// let num = 206;
+// let sum = 0;
+// while(num<311){
+    // console.log(num)
+//     num = num+2;
+//     sum = sum+num;
+// }
+
+// console.log(sum)
+
+
+
+// 3.
+// let num = 5;
+// while(num<=125){
+//     console.log (num)
+//     num = num+5
+//     num*5
+// }
+
+
+
+4.
+
+
+// let num =21;
+// while(num>=15){
+//     console.log(num)
+//     num--
+// }
+
+
+// 6.
+
+
+// let arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+// let arrNew = [];
+
+// for ( let i = 0; i<arr.length; i++)
+// {
+//     let updatedI = arr[i]*2;
+// //    console.log(updatedI)
+//    arrNew.push(updatedI)
+// }
+// console.log(arrNew)
