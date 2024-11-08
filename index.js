04e98edd3c278823358d9c52d428082c7e224730
jahid9982 <rayhan99982@gmail.com>
2024-11-08 19:52:48 +0600

    first commit

--- index.js
+++ index.js
@@ -60,7 +60,24 @@
 // }
 
 
-// 6.
+5.
+
+
+// let numFirst =0;
+// for(let num = 1 ; num<=500; num++){
+//     if(num%3 == 0){
+//         console.log(num)
+//     }
+// }
+let numFirst =0;
+for(let num = 1 ; num<=500; num++){
+    if(num%5 == 0){
+        console.log(num)
+    }
+}
+
+
+// 6. 
 
 
 // let arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
